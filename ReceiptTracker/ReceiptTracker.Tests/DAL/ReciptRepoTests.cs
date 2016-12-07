47857667e78b0d3727da459caeee9a0f2fc757a7
Bradley Guthrie <guppy96@gmail.com>
2016-12-07 11:22:25 -0600

    project created, new models and DAL stub and test of Repo started

---
+++ ReceiptTracker/ReceiptTracker.Tests/DAL/ReciptRepoTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReceiptTracker.Tests.DAL
+{
+    [TestClass]
+    public class ReciptRepoTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ReceiptTracker/ReceiptTracker/DAL/ReceiptContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReceiptTracker.DAL
+{
+    public class ReceiptContext
+    {
+    }
+}
