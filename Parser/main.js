6ca9d8534f735dd64833a675d811425ae890bc62
Bradley Guthrie <guppy96@gmail.com>
2017-01-06 12:44:47 -0600

    updated gitignore to not wath node-modules and shell for parser JS

--- Parser/main.js
+++ Parser/main.js
@@ -1,13 +1,16 @@
 var MailParser = require("mailparser").MailParser;
 var mailparser = new MailParser();
-
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
-
 var bucketName = 'guthbtestemail';
+var http = require('http');
+var options = {
+    host: 'http://receipttracker20170105062952.azurewebsites.net',
+    path: 'api/user/'
+    };
+var q = require('q');
 
-//sample JSON
-
+//sample json
 /*{
    "receiptCapturedId": 1,
    "receiptType": "html",
@@ -29,10 +32,31 @@ mailparser.on("end", function(mail_object){
     receipt.purchaseDate = new Date();
     receipt.s3BuckedId = 1;
     receipt.purpose = "unknown"
-
-
-
 });
+
+
+/* need to include promises that insure that the email is complely streamed into
+the mailparser as well as the api for passing in the app email and gettign back
+the user Id so the reciep can be posted to the database 
+*/
+function getUserId( userName ) {
+    callback = function(response) {
+      var str = '';
+
+      //another chunk of data has been recieved, so append it to `str`
+      response.on('data', function (chunk) {
+        str += chunk;
+      });
+
+      //the whole response has been recieved, so we just print it out here
+      response.on('end', function () {
+        console.log(str);
+      });
+    }
+
+    http.request(options, callback).end();
+}
+
 
 exports.handler = function(event, context, callback) {
     console.log('Process email');
